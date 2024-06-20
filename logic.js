e27b0ab09c17122c683854c88337194e68f15fd3
venkatesh <venkatesh.p@plugxr.com>
2024-06-20 18:36:20 +0530

    loaders

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.min.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.154.0/examples/jsm/loaders/GLTFLoader.js";
 let loader = new GLTFLoader();
 let sleighModel;
 loader.load("./assets/player.glb", (gltf) => {
